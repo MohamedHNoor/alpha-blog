76cbf372b12eb933568a8b194f230ac9a0ea3f67
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-08 02:05:00 +0200

    Add new and create actionscontrollers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+    redirect_to @article # article_path(@article)
+    else 
+      render 'new'
+    end
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
